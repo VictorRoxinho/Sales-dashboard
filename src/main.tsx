a32bdff75e2896830be430e563e6c159f8b59900
VictorRoxinho <161038151+VictorRoxinho@users.noreply.github.com>
2025-02-07 09:53:39 -0300

    Revert "Feat: Create Themes"

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import { StrictMode } from 'react'
-import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { GlobalStyle, lightTheme } from './styles'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 )
